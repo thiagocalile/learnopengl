e1b5b34288a438017f1cc4c10e763e374e5fab77
Thiago <thiagoc@usp.br>
2026-08-22 18:18:07 -0300

    Dá uma geral na infraestrutura

--- flake.nix
+++ flake.nix
@@ -5,11 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = inputs: {
-    packages = builtins.mapAttrs (system: pkgs: {
-      glfw = pkgs.glfw;
-
-      default = inputs.self.packages.${system}.glfw;
-    }) inputs.nixpkgs.legacyPackages;
-  };
+  outputs = { self, nixpkgs } :
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      in {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ cmake pkg-config ];
+        buildInputs = with pkgs; [ glfw libGL ];
+      };
+    };
 }
